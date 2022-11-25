e1f833470271b07dee710842edc773f1741d96ff
lykmast <lykmast@gmail.com>
2022-11-25 13:29:23 +0100

    Modify smt_app to solve any extra goals that might appear.

--- src/Preamble.hs
+++ src/Preamble.hs
@@ -7,6 +7,6 @@ preamble = natDecl : ltacs
     natDecl    = "Inductive PNat:Set := Z : PNat | S: PNat -> PNat."
     ple        = "Ltac ple := simpl."
     smtTrivial = "Ltac smt_trivial := simpl; first [ assumption | intuition discriminate | easy]."
-    smtApp     = "Ltac smt_app th := first "   ++ appTacList ++ "."
+    smtApp     = "Ltac smt_app th := first "   ++ appTacList ++ "; try smt_trivial."
     smtSolve   = "Ltac smt_solve th := solve [ now rewrite th | now ple; rewrite th | now apply th | now ple; apply th]."
     appTacList = "[ rewrite th | ple; rewrite th | apply th | ple; apply th]"
